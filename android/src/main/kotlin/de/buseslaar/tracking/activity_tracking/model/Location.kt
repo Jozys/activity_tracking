97692918ed780953f3de9f92fdb0736fa77f0485
Jozys <joshua@slaar.de>
2025-03-13 16:10:52 +0100

    🐛 Add missing Location data class
    Introduce a `Location` data class to represent latitude and longitude coordinates.

---
+++ android/src/main/kotlin/de/buseslaar/tracking/activity_tracking/model/Location.kt
@@ -0,0 +1,14 @@
+package de.buseslaar.tracking.activity_tracking.model
+
+class Location {
+
+    private var latitude : Double = 0.0;
+    private var longitude : Double = 0.0;
+
+    constructor(latitude: Double, longitude: Double) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+
+}

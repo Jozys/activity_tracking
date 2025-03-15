3efc7675a1f27fe251801a050071930760cd16b2
Jozys <joshua@slaar.de>
2025-03-15 15:17:15 +0100

    ♻️ Removed unnecessary semicolons in Kotlin

--- android/src/main/kotlin/de/buseslaar/tracking/activity_tracking/sensor/SensorManager.kt
+++ android/src/main/kotlin/de/buseslaar/tracking/activity_tracking/sensor/SensorManager.kt
@@ -9,31 +9,33 @@ import android.util.Log
 
 open class SensorManager {
 
-    private var sensorManager : SensorManager? = null
+    private var sensorManager: SensorManager? = null
     private var sensor: Sensor? = null
-    private val TAG = "SensorManager";
+    private val TAG = "SensorManager"
     var onSensorChangedListener: (SensorEvent) -> Unit = {}
 
     constructor(context: Context, sensorType: Int) {
-        this.sensorManager = context.getSystemService(Context.SENSOR_SERVICE ) as SensorManager;
-        this.sensor = sensorManager?.getDefaultSensor(sensorType);
+        this.sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        this.sensor = sensorManager?.getDefaultSensor(sensorType)
     }
 
     fun startListening() {
-        val supportedAndEnabled = sensorManager?.registerListener(listener,
-            sensor, SensorManager.SENSOR_STATUS_ACCURACY_HIGH)
+        val supportedAndEnabled = sensorManager?.registerListener(
+            listener,
+            sensor, SensorManager.SENSOR_STATUS_ACCURACY_HIGH
+        )
         Log.d(TAG, "Sensor listener registered: $supportedAndEnabled")
     }
 
     fun stopListening() {
-        sensorManager?.unregisterListener(listener);
+        sensorManager?.unregisterListener(listener)
     }
 
     val listener: SensorEventListener by lazy {
         object : SensorEventListener {
             override fun onSensorChanged(event: SensorEvent?) {
                 if (event == null) return
-                onSensorChangedListener(event);
+                onSensorChangedListener(event)
             }
 
             override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
--- android/src/main/kotlin/de/buseslaar/tracking/activity_tracking/sensor/StepSensor.kt
+++ android/src/main/kotlin/de/buseslaar/tracking/activity_tracking/sensor/StepSensor.kt
@@ -5,20 +5,23 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 
 
-class StepSensor: SensorManager {
+class StepSensor : SensorManager {
 
-    private val TAG = "StepSensor";
+    private val TAG = "StepSensor"
     private var onStepChangedListener: (addedSteps: Int) -> Unit = {}
 
     init {
         super.onSensorChangedListener = ({ onSensorChangedListener(it) })
     }
 
-    constructor(context: Context, onStepChanged: (addedSteps: Int) -> Unit) : super(context, Sensor.TYPE_STEP_DETECTOR) {
-        this.onStepChangedListener = onStepChanged;
+    constructor(context: Context, onStepChanged: (addedSteps: Int) -> Unit) : super(
+        context,
+        Sensor.TYPE_STEP_DETECTOR
+    ) {
+        this.onStepChangedListener = onStepChanged
     }
 
-    private fun onSensorChangedListener(event: SensorEvent)  {
+    private fun onSensorChangedListener(event: SensorEvent) {
         val stepsSinceLastReboot = event.values
         onStepChangedListener(stepsSinceLastReboot[0].toInt())
     }
